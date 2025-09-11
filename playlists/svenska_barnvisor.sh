635de1f9984445bae9d5318de6caf33ae5d883cb
Mousa Zeid Baker <moubak@norlys.dk>
2025-09-11 14:09:04 +0200

    feat: add more playlists

---
+++ playlists/svenska_barnvisor.sh
@@ -0,0 +1,20 @@
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:00:14 -to 00:01:52 "Lilla Snigel".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:02:38 -to 00:03:56 "Fem Små Apor Hoppade i Sängen".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:04:40 -to 00:04:49 "Näsduksspöket".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:05:25 -to 00:06:52 "Klappa Händerna När Du Är Riktigt Glad".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:07:30 -to 00:09:01 "Imse Vimse Spindel".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:09:22 -to 00:10:22 "En Kulen Natt".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:12:02 -to 00:13:12 "Tingelingetåget Far".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:13:48 -to 00:15:44 "Hjulen På Bussen".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:16:42 -to 00:18:13 "Fem Fina Fåglar".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:19:40 -to 00:21:15 "En Sockerbagare".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:22:45 -to 00:25:12 "Bä Bä Vita Lamm".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:25:31 -to 00:26:36 "Hajarna".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:28:34 -to 00:30:12 "Björnen Sover".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:33:55 -to 00:06:49 "Vipp"-På-Rumpan-Affärn.m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:37:50 -to 00:39:23 "Små Grodorna".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:40:28 -to 00:41:58 "Krokodilen i Bilen".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:42:10 -to 00:43:25 "Tänk Om Jag Hade En Liten Liten Apa".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:46:52 -to 00:48:51 "Finger familjen".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:52:30 -to 00:53:44 "Baka Baka Liten Kaka".m4a
+ffmpeg -i input.m4a -vn -acodec copy -ss 00:57:15 -to 00:59:40 "Blinka Lilla Stjärna Där".m4a
